531dc4cabec4294610e7afae4ef275d791bc5cc4
oweneustice <owen.eustice@moshmaker.com>
2013-08-19 13:02:15 -0700

    action test

---
+++ action.php
@@ -0,0 +1,6 @@
+Hello, <?php echo htmlspecialchars($_POST['first_name']); ?>.
+<br/>
+You mentioned your last name was
+<?php echo htmlspecialchars($_POST['last_name']); ?>, correct?
+<br/><br/>
+<a href="test.php">Back</a>
--- test.php
+++ test.php
@@ -1,3 +1,8 @@
 <?php
 echo "My first PHP script! Now with all new text!!";
 ?>
+<form action="action.php" method="post">
+	<p>First Name: <input type="text" name="first_name" /></p>
+	<p>Last Name: <input type="text" name="last_name" /></p>
+	<p><input type="submit" /></p>
+</form>
